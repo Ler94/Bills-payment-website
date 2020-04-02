4ce50d9941380b1a92c200e062cfb5f52b0b48c6
Valeria <62205078+Ler94@users.noreply.github.com>
2020-04-02 13:43:36 +0300

    Update settings.py

--- PAY/settings.py
+++ PAY/settings.py
@@ -28,8 +28,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'estrella4494@gmail.com'
-EMAIL_HOST_PASSWORD = 'notrelera94'
+EMAIL_HOST_USER = '***'
+EMAIL_HOST_PASSWORD = '*****'
 ALLOWED_HOSTS = []
 
 
